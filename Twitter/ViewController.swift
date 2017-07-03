5f8262556bcf215a5ae33e2302d1e4cebdb38849
Shao Soh <sysoh@macbook-pro-134.dhcp.thefacebook.com>
2017-07-03 13:42:18 -0700

    Initial Commit

---
+++ Twitter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Twitter
+//
+//  Created by Shao Yie Soh on 7/3/17.
+//  Copyright © 2017 Shao Yie Soh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
